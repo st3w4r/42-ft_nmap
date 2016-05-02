ac52ece32da7698b7194fc84eda983ac3b1b14fb
Yanael Barbier <ybarbier@student.42.fr>
2016-05-02 18:34:57 +0200

    Add get file in init options

--- srcs/init_options.c
+++ srcs/init_options.c
@@ -57,7 +57,7 @@ int nm_init_file_opt(char *arg)
 {
   ft_putstr("nm_init_file_opt\n");
   printf("argtype: %s\n", arg);
-
+  nm_get_ip_file(arg);
   return (0);
 }
 int nm_init_speedup_opt(char *arg)
