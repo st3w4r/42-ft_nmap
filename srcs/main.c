ae4c1d775309e24d97ece34cef5e602f33196c60
Yanael Barbier <ybarbier@student.42.fr>
2016-05-20 15:29:41 +0200

    Add root check

--- srcs/main.c
+++ srcs/main.c
@@ -3,7 +3,8 @@
 /* Ne pas oublier de remettre les Flags */
 int               main(int argc, char **argv)
 {
-
+	if (getuid() != 0)
+		ft_error_str_exit("ft_nmap: Operation not permitted\n");
   (void)argc;
   nm_g_struct_init();
   nm_argv_parser(argv, argc);
