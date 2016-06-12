1cfe8a1397b0b884464143eb38d0bd9250389ece
Yanael Barbier <st3w4r@gmail.com>
2016-06-12 22:53:57 +0200

    Add getservbyport

--- srcs/services.c
+++ srcs/services.c
@@ -2,25 +2,9 @@
 
 char	*nm_get_service_name(int port, char *protocol)
 {
-	int	i;
-	t_service	services_list[]  =
-	{
-		{ "ftp", 21, "tcp" },
-		{ "ftp", 21, "udp" },
-		{ "http", 80, "tcp" },
-		{ "http", 80, "udp" },
-		{ "https", 443, "tcp" },
-		{ "https", 443, "udp" },
-		{ 0, 0, 0 }
-	};
-
-	i = 0;
-	while (services_list[i].name != NULL)
-	{
-		if (services_list[i].port == port &&
-				ft_strcmp(protocol, services_list[i].protocol) == 0)
-			return (services_list[i].name);
-		i++;
-	}
-	return ("Unassigned");
+	struct servent *service;
+	service = getservbyport(htons(port), protocol);
+	if (service == NULL)
+		return ("Unassigned");
+	return (service->s_name);
 }
